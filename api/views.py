544bd657795c0f8cb61ce34da4d251baf538a4e3
Momoh Nobert <okhai0112@gmail.com>
2020-07-12 22:36:17 +0100

    added user authentification and token generation, more convenient implementation.

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -9,15 +9,14 @@ from .serializers import MovieSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, )
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            # user = request.user
-            user = User.objects.get(id=1)
-            print('User :', user.username)
+            user = request.user
 
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
@@ -39,3 +38,4 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication,)
