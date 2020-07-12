3e552d9660da0a97c5b81151a631cf833a7a93af
Momoh Nobert <okhai0112@gmail.com>
2020-07-12 23:22:37 +0100

    created register user logic. made adjustments to serializers, urls and views

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from .models import Movie, Rating
-from .serializers import MovieSerializer, RatingSerializer
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
